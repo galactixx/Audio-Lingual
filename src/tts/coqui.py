47fe4b0f350f1b56a3adf3296bd7ad8fb1c4d465
galactixx <nwlezien.github@gmail.com>
2023-10-31 02:20:50 -0500

    whoops, coqui has a model manager, adding that in

--- src/tts/coqui.py
+++ src/tts/coqui.py
@@ -1,10 +1,17 @@
 from TTS.api import TTS
 import pyaudio
+import json
+from threading import Thread
 
-from src.utils import coqui_gather_and_download_model_by_selection
 from src.tts.base import BaseTTS
+from TTS.utils.manage import ModelManager
 from src.models.models import CoquiModelGroup, TTSModels
 from src.models.language_codes import LanguageCodes
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+MODEL_DIRECTORY = config['downloaded_models_path']
 
 class Coqui(BaseTTS):
     """Interface for Coqui voice generation."""
@@ -17,6 +24,18 @@ class Coqui(BaseTTS):
         self.model_group = model_group
 
         # Based on specified model, download model if it needs to be downloaded
-        coqui_gather_and_download_model_by_selection(model=self.model,
-                                                     language=self.model,
-                                                     model_gorup=self.model_group)
+        manager = ModelManager()
+
+        # Use the ModelManager to download a model
+        model_path, config_path, model_item = manager.download_model(
+            f'tts_models/{self.language}/{self.model_group}/{self.model}',
+            out_path=MODEL_DIRECTORY,
+            progress_bar=True)
+
+    def _voice_generation_threaded(self, text: str) -> None:
+        """Generate final voice generation based on text input."""
+        pass
+
+    def voice_generation(self, text: str) -> Thread:
+        """Generate thread of voice generation function."""
+        pass
